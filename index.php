d680945d3848c5f872079f51461320e1b9ec287f
robertvlad <vladrob96@gmail.com>
2023-02-08 17:29:11 +0100

    Creata lista di film

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+    class Movie{
+
+        //Attributi d'istanza
+        public $title;
+        public $year;
+        public $genre;
+
+        //COSTRUTTORE
+        public function __construct($title, $year, $genre)
+        {
+            $this->title = $title;
+            $this->year = $year;
+            $this->genre = $genre;            
+        }
+
+        //METODO STAMPA INFORMAZIONI DELLA CLASSE MOVIE
+        public function getMovieInfo()
+        {
+            return $this->title." - ".$this->year." - ".$this->genre;
+        }
+    }
+    
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <title>Document</title>
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <?php
+                        $harrypotter1 = new Movie ("Harry potter e la pietra filosofale", "2001", "fantasy");
+                        $harrypotter2 = new Movie ("Harry potter e la camera dei segreti", "2002", "fantasy");
+                        $harrypotter3 = new Movie ("Harry potter e il prigioniero di Azkabanm", "2004", "fantasy");
+                        $harrypotter4 = new Movie ("Harry potter e il calice di fuoco", "2005", "fantasy");
+                        $harrypotter5 = new Movie ("Harry potter e l'ordine della fenice", "2007", "fantasy");
+                        $harrypotter6 = new Movie ("Harry potter e il principe mezzo-sangue", "2009", "fantasy");
+                        $harrypotter7parte1 = new Movie ("Harry potter e i doni della morte: Parte 1", "2010", "fantasy");
+                        $harrypotter7parte2 = new Movie ("Harry potter e i doni della morte: Parte 2", "2011", "fantasy");
+
+                        echo $harrypotter1->getMovieInfo();
+                        echo "<br>";
+                        echo $harrypotter2->getMovieInfo();
+                        echo "<br>";
+                        echo $harrypotter3->getMovieInfo();
+                        echo "<br>";
+                        echo $harrypotter4->getMovieInfo();
+                        echo "<br>";
+                        echo $harrypotter5->getMovieInfo();
+                        echo "<br>";
+                        echo $harrypotter6->getMovieInfo();
+                        echo "<br>";
+                        echo $harrypotter7parte1->getMovieInfo();
+                        echo "<br>";
+                        echo $harrypotter7parte2->getMovieInfo();
+                        echo "<br>";
+                    ?>                    
+                </div>
+            </div>
+        </div>        
+    </body>
+
+    <script src=""></script>
+</html>
